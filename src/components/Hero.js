e1c1f1e4a401ce3e06abd59df01e342e32b8a366
AlbbercaGit <alejandroalbbercabarcelona@gmail.com>
2024-08-08 13:05:34 +0200

    A poner textos

--- src/components/Hero.js
+++ src/components/Hero.js
@@ -14,10 +14,10 @@ const Hero = () => {
     <main className="max-h-[100vh]">
       <div className=" h-[100vh] text-white w-full flex items-center justify-center flex-col  relative top-0 font-sans">
         <div className="flex flex-col w-full h-full items-center justify-center">
-          <h1 className="text-white text-5xl flex h-full w-full items-end justify-center m-10">
+          <h1 className="text-white text-3xl flex h-full w-full items-end justify-center m-10">
             ¿Y ahora que?
           </h1>
-          <nav className="text-white flex h-full w-full justify-center items-start text-4xl gap-10">
+          <nav className="text-white flex h-full w-full justify-center items-start text-2xl gap-10">
             <button className="font-sans py-2 px-4 underline-button2">Reservas</button>
             <button className="font-sans py-2 px-4 underline-button2">Carta</button>
           </nav>
--- src/components/Sticky.js
+++ src/components/Sticky.js
@@ -1,21 +1,49 @@
 import React from "react";
-import { motion, useScroll, useTransform } from "framer-motion";
+import { motion, useScroll, useTransform , useMotionTemplate } from "framer-motion";
 import "../style.css";
 
 const Sticky = () => {
-  const SECTION_HEIGHT = 1500;
+  const SECTION_HEIGHT = 1000;
 
   const { scrollY } = useScroll();
-  const opacity = useTransform(scrollY, [SECTION_HEIGHT, SECTION_HEIGHT + 500], [1, 0]);
-  const backgroundSize = useTransform(scrollY, [0, 500], ["170%", "100%"]);
+  const opacity = useTransform(
+    scrollY,
+    [SECTION_HEIGHT, SECTION_HEIGHT + 100],
+    [1, 0]
+  );
+  
+  const clip1 = useTransform(
+    scrollY,
+    [0, SECTION_HEIGHT *3 ],
+    [25, 0]
+  );
+  const clip2 = useTransform(
+    scrollY,
+    [0, SECTION_HEIGHT *3],
+    [75, 100]
+  );
+
+     const clipPath = useMotionTemplate`polygon(${clip1}% ${clip1}%, ${clip2}% ${clip1}%, ${clip2}% ${clip2}%, ${clip1}% ${clip2}%)`;
+
+  
+
+
+  // const backgroundSize = useTransform(
+  //   scrollY,
+  //   [0, SECTION_HEIGHT + 500],
+  //   ["170%", "100%"]
+  // );
 
   return (
-    <main className="relative w-full h-[150vh]">
+    <main className="relative w-full h-[350vh] bg-paper-texture">
       <motion.div
-        className="sticky top-0 h-screen w-full bg-izq bg-cover bg-center bg-no-repeat"
+        className="sticky top-0 h-screen w-full  bg-izq bg-cover bg-center bg-no-repeat "
         style={{
-          opacity,
-        //   backgroundSize,
+          // opacity,
+          // clipPath,
+          // backgroundSize,
+          clipPath: "polygon(50% 25%, 75% 25%, 75% 75%, 25% 75%)",
+       clipPath,
           backgroundAttachment: "fixed",
           // backgroundImage: "url(../public/img/limon.jpg)",
         }}
