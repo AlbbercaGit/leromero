cc7fd222e1a75558d252a2209ff8a686a9ab5422
AlbbercaGit <alejandroalbbercabarcelona@gmail.com>
2024-08-08 13:16:22 +0200

    quiero poner texto:c

--- src/components/Sticky.js
+++ src/components/Sticky.js
@@ -1,5 +1,5 @@
-import React from "react";
-import { motion, useScroll, useTransform , useMotionTemplate } from "framer-motion";
+import React, { useEffect } from "react";
+import { motion, useScroll, useTransform, useMotionTemplate } from "framer-motion";
 import "../style.css";
 
 const Sticky = () => {
@@ -11,41 +11,38 @@ const Sticky = () => {
     [SECTION_HEIGHT, SECTION_HEIGHT + 100],
     [1, 0]
   );
-  
+
   const clip1 = useTransform(
     scrollY,
-    [0, SECTION_HEIGHT *3 ],
+    [0, SECTION_HEIGHT * 3],
     [25, 0]
   );
   const clip2 = useTransform(
     scrollY,
-    [0, SECTION_HEIGHT *3],
+    [0, SECTION_HEIGHT * 3],
     [75, 100]
   );
 
-     const clipPath = useMotionTemplate`polygon(${clip1}% ${clip1}%, ${clip2}% ${clip1}%, ${clip2}% ${clip2}%, ${clip1}% ${clip2}%)`;
+  const clipPath = useMotionTemplate`polygon(${clip1}% ${clip1}%, ${clip2}% ${clip1}%, ${clip2}% ${clip2}%, ${clip1}% ${clip2}%)`;
 
-  
+  useEffect(() => {
+    const handleScroll = () => {
+      const parallax = document.querySelector(".parallax");
+      const scrollPosition = window.pageY;
+      parallax.style.backgroundPositionY = `${scrollPosition * 0.5}px`;
+    };
 
-
-  // const backgroundSize = useTransform(
-  //   scrollY,
-  //   [0, SECTION_HEIGHT + 500],
-  //   ["170%", "100%"]
-  // );
+    window.addEventListener("scroll", handleScroll);
+    return () => window.removeEventListener("scroll", handleScroll);
+  }, []);
 
   return (
-    <main className="relative w-full h-[350vh] bg-paper-texture">
+    <main className="relative w-full h-[380vh] bg-paper-texture">
       <motion.div
-        className="sticky top-0 h-screen w-full  bg-izq bg-cover bg-center bg-no-repeat "
+        className="sticky top-0 h-screen w-full parallax bg-izq bg-cover bg-center bg-no-repeat"
         style={{
-          // opacity,
-          // clipPath,
-          // backgroundSize,
-          clipPath: "polygon(50% 25%, 75% 25%, 75% 75%, 25% 75%)",
-       clipPath,
-          backgroundAttachment: "fixed",
-          // backgroundImage: "url(../public/img/limon.jpg)",
+          clipPath,
+          willChange: "clip-path, opacity",
         }}
       ></motion.div>
     </main>
