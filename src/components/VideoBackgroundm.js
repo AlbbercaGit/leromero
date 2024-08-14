81b405e30599ee26da38cbaf8688c5923757326b
Albbberca <alejandroalbbercabarcelona@gmail.com>
2024-08-14 11:42:31 +0200

    Cambio de fondo

--- src/components/VideoBackgroundm.js
+++ src/components/VideoBackgroundm.js
@@ -2,7 +2,8 @@
 import React from 'react';
 
 const VideoBackground = () => {
-  const videoUrl = 'https://raw.githubusercontent.com/AlbbercaGit/videos/main/leromerm.mp4'; // URL directa al archivo de video
+  // const videoUrl = 'https://raw.githubusercontent.com/AlbbercaGit/videos/main/leromerm.mp4'; 
+  const videoUrl = 'https://raw.githubusercontent.com/AlbbercaGit/videos/a00d2c213e92e058f86440696638e32366b4f06f/500b89663c9ecc19b2c91a85da13735c.webm'; 
 
   return (
     <div className="absolute top-0 left-0 w-full h-[110vh] block md:hidden">
